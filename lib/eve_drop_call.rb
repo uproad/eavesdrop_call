7685b8b43d2a557584e6f43ec0706ccce0194fcd
uproad krowd <7349115+uproad@users.noreply.github.com>
2018-05-13 22:34:17 +0900

    [add] It is completely what I want to do.
    It logs all of target class's method calling.
    But it write STDOUT.
    
    HOW TO USE: EveDropCall.eavesdrop(TargetKlass)

--- lib/eve_drop_call.rb
+++ lib/eve_drop_call.rb
@@ -1,5 +1,42 @@
 require "eve_drop_call/version"
 
 module EveDropCall
-  # Your code goes here...
+  class << self
+    def eavesdrop(_target_klass)
+      _i, _c = _prepend_gem_module(_target_klass)
+      _target_klass.prepend(_i)
+      _target_klass.singleton_class.prepend(_c)
+
+      _target_klass
+    end
+
+    private
+
+    def _prepend_gem_module(_target)
+      _instance_methods = _target.instance_methods
+      _class_methods = _target.singleton_class.instance_methods
+
+      _instance_methods_module = Module.new do
+        _instance_methods.each do |_method|
+          next unless _method[0] =~ /[a-z|A-Z]/
+          define_method(_method) do |*args, &block|
+            puts "PrependMethods##{_method}"
+            super(*args, &block)
+          end
+        end
+      end
+
+      _class_methods_module = Module.new do
+        _class_methods.each do |_method|
+          next unless _method[0] =~ /[a-z|A-Z]/
+          define_method(_method) do |*args, &block|
+            puts "PrependMethods.#{_method}"
+            super(*args, &block)
+          end
+        end
+      end
+
+      return _instance_methods_module, _class_methods_module
+    end
+  end
 end
