6b919077f7d1d334606d8f2d616eb29e1e6c6576
uproad krowd <7349115+uproad@users.noreply.github.com>
2018-05-14 13:56:33 +0900

    [test][create] a basic expected.

--- spec/eve_drop_call_spec.rb
+++ spec/eve_drop_call_spec.rb
@@ -3,7 +3,35 @@ RSpec.describe EveDropCall do
     expect(EveDropCall::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#eavesdrop' do
+    subject(:eavesdrop){ EveDropCall.eavesdrop(target) }
+    let(:target) do
+      Class.new do
+        class << self
+          def x
+            Callstack.puts "Target.x"
+            new.x
+          end
+        end
+
+        def x
+          Callstack.puts "Target#x"
+        end
+      end
+    end
+
+    it{ is_expected.to eq target }
+
+    describe 'can eavesdrop?' do
+      before{ eavesdrop }
+      subject{ target.x }
+
+      # p = PrependKlass
+      # t = TargetKlass
+
+      # marked call stack is expected
+      # #=> p.x > t.x > p#new > p#x > t#x
+      it { expect{ target.x }.to change{ Callstack.list.size }.to(5) }
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require "bundler/setup"
 require "eve_drop_call"
 
+require 'callstack'
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -10,5 +12,10 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.after(:each) do
+    Callstack.dump
+    Callstack.reset
   end
 end
