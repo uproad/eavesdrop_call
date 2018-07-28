db1434d8d28e14dc311ac5505e9c787f8f9fddf6
uproad krowd <7349115+uproad@users.noreply.github.com>
2018-07-29 03:03:27 +0900

    [remove] 変数の '_' を削除
    ノリで付けたけどいらない気がする

--- lib/eve_drop_call.rb
+++ lib/eve_drop_call.rb
@@ -5,49 +5,49 @@ require 'fileutils'
 
 module EveDropCall
   LOGGER = Callstack
-  @@_target_klass_name = nil
+  @@target_klass_name = nil
 
   class << self
-    def eavesdrop(_target_klass, _called_dir_path = File.expand_path(File.dirname(__FILE__)))
-      @@_target_klass_name = _target_klass.to_s
-      @@current_dir = _called_dir_path
+    def eavesdrop(target_klass, called_dir_path = File.expand_path(File.dirname(__FILE__)))
+      @@target_klass_name = target_klass.to_s
+      @@current_dir = called_dir_path
 
-      _i, _c = _prepend_gem_module(_target_klass)
-      _target_klass.prepend(_i)
-      _target_klass.singleton_class.prepend(_c)
+      instance_methods, class_methods = prepend_gem_module(target_klass)
+      target_klass.prepend(instance_methods)
+      target_klass.singleton_class.prepend(class_methods)
 
-      _target_klass
+      target_klass
     end
 
     private
 
-    def _prepend_gem_module(_target)
-      _instance_methods = _target.instance_methods
-      _class_methods = _target.singleton_class.instance_methods
+    def prepend_gem_module(target)
+      instance_methods = target.instance_methods
+      class_methods = target.singleton_class.instance_methods
 
-      _instance_methods_module = Module.new do
-        _instance_methods.each do |_method|
-          next unless _method[0] =~ /[a-z|A-Z]/
-          define_method(_method) do |*args, &block|
-            trace_first = caller.find{|p| p.start_with?(@@current_dir) && !p.include?("spec_helper") }
-            LOGGER.push({ method_name: "#{_method} [I]", last_call_line: trace_first }) if trace_first
+      instance_methods_module = Module.new do
+        instance_methods.each do |method|
+          next unless method[0] =~ /[a-z|A-Z]/
+          define_method(method) do |*args, &block|
+            trace_first = caller.find{|p| p.start_with?(@@current_dir) }
+            LOGGER.push({ method_name: "#{method} [I]", last_call_line: trace_first }) if trace_first
             super(*args, &block)
           end
         end
       end
 
-      _class_methods_module = Module.new do
-        _class_methods.each do |_method|
-          next unless _method[0] =~ /[a-z|A-Z]/
-          define_method(_method) do |*args, &block|
-            trace_first = caller.find{|p| p.start_with?(@@current_dir) && !p.include?("spec_helper") }
-            LOGGER.push({ method_name: "#{_method} [C]", last_call_line: trace_first }) if trace_first
+      class_methods_module = Module.new do
+        class_methods.each do |method|
+          next unless method[0] =~ /[a-z|A-Z]/
+          define_method(method) do |*args, &block|
+            trace_first = caller.find{|p| p.start_with?(@@current_dir) }
+            LOGGER.push({ method_name: "#{method} [C]", last_call_line: trace_first }) if trace_first
             super(*args, &block)
           end
         end
       end
 
-      return _instance_methods_module, _class_methods_module
+      return instance_methods_module, class_methods_module
     end
   end
 end
